ba216dbec0d47c6ecf8055497d237c72aad3c052
Ricky LeDew <adotout1@gmail.com>
2022-05-22 19:30:01 -0400

    Icons

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -17,13 +17,21 @@ const Home: React.FC<HomeProps> = () => {
     return (
         <div className="page home">
             <div className="content">
+                <div className='section intro'>
                     <div className="hero">
                         <h1>Patrick LeDew</h1>
                         <h2 className='text-muted'>A computer science student studying at UNF</h2>
                     </div>
-                <div className='section'>
-                    <h1>Projects</h1>
-                    <p>{text}</p>
+                    <div className="icons">
+                        <div><a href="https://www.linkedin.com/in/patrick-ledew/" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-5x fa-linkedin"></i></a></div>
+                        <div><a href="https://github.com/patrickledew" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-5x fa-github-square"></i></a></div>
+                    </div>
+                </div>
+                <div className='section move about'>
+                    <div className='section-content'>
+                        <h1>About Me</h1>
+                        <p>{text}</p>
+                    </div>
                 </div>
             </div>
         </div>
