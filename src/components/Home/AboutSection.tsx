e15eed58fcbd9d945784dbdff034cfecd973490c
Ricky LeDew <adotout1@gmail.com>
2022-05-27 04:06:52 -0400

    Made layout responsive

--- src/components/Home/AboutSection.tsx
+++ src/components/Home/AboutSection.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import Me from "../../res/me.jpg";
+import "./aboutSection.scss";
 const AboutSection: React.FC = () => {
   let [text, setText] = React.useState("");
   React.useEffect(() => {
@@ -18,22 +19,26 @@ const AboutSection: React.FC = () => {
   return (
     <div className="section about">
       <div className="section-content move">
-        <h1>About Me</h1>
-        <p>
-          Hi! My name is Patrick, but most people call me Ricky. I'm a 19 year
-          old computer science student located in Jacksonville, FL, currently
-          attending the University of North Florida.
-        </p>
-        <p>
-          I've been interested in computers for as long as I can remember, and
-          I've been captivated by the puzzle-like quality of programming since
-          my early teens. I love making websites, games, experiments, and
-          everything in between. Now I'm looking to help solve puzzles in the
-          real world.
-        </p>
-        <p></p>
+        <div className="about-text">
+          <h1>About Me</h1>
+          <p>
+            Hi! My name is Patrick, but most people call me Ricky. I'm a 19 year
+            old computer science student located in Jacksonville, FL, currently
+            attending the University of North Florida.
+          </p>
+          <p>
+            I've been interested in computers for as long as I can remember, and
+            I've been captivated by the puzzle-like quality of programming since
+            my early teens. I love making websites, games, experiments, and
+            everything in between. Now I'm looking to help solve puzzles in the
+            real world.
+          </p>
+          <p></p>
+        </div>
+        <div className="selfie">
+          <img alt="A picture of me" src={Me} />
+        </div>
       </div>
-      <img className="selfie" alt="A picture of me" src={Me} height="400px" />
     </div>
   );
 };
